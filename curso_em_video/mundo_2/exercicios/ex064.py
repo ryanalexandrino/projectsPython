866449604497cccbbdd824c699534dedcfd7e96d
Ryan Alexandrino <ryansoftdev@gmail.com>
2025-08-02 12:38:28 -0300

    Ajuste de exercícios
    Ajuste simples no exercício 64.

--- curso_em_video/mundo_2/exercicios/ex064.py
+++ curso_em_video/mundo_2/exercicios/ex064.py
@@ -4,9 +4,9 @@
 # Desconsidere o valor 999 nos dois casos
 
 # Inicializa as variáveis globais
-numero = 0
 contador = 0
 soma = 0
+numero = int(input('Digite um número: '))
 
 # Cria o laço que executa o programa
 while numero != 999:
@@ -14,5 +14,5 @@ while numero != 999:
     contador += 1
     soma += numero
 print(f'\nO programa foi encerrado devido a inserção do valor "999"! \n'
-      f'Você digitou um total de {contador - 1} números \n'
-      f'A soma desses número é igual a: {soma - 999}')
+      f'Você digitou um total de {contador} números \n'
+      f'A soma desses número é igual a: {soma}')
